8392afcb39a3c441c312f7abf03985c31688a3a4
Asrielbot <49340417+Asrielbot@users.noreply.github.com>
2023-01-09 13:08:38 +0100

    Méthode ajouter championnat

--- gestionChampionnat.py
+++ gestionChampionnat.py
@@ -8,8 +8,14 @@ class GestionChampionnat:
         print("2. Afficher les championnats")
         print("3. Quitter")
     
-    def ajouterChampionnat(self, championnat):
+    def ajouterChampionnat(self, championnat, nom, pays, nb_equipes, point_win, point_nul, point_lose, ex_aequo):
         self.championnats.append(championnat)
+        self.nom = str(input("Nom du championnat : "))
+        self.pays = str(input("Pays du championnat : "))
+        self.nb_equipes = int(input("Nombre d'équipes : "))
+        self.point_win = int(input("Points pour une victoire : "))
+        self.point_nul = int(input("Points pour un match nul : "))
+        self.point_lose = int(input("Points pour une défaite : "))
     
     def ajouterEquipe(self, equipe):
         self.championnats.append(equipe)
