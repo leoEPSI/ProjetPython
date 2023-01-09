8e856ba3d6689af59161625557d8295e5cf38c1f
CodingStrqf <l.ruy42@orange.fr>
2023-01-09 16:48:56 +0100

    affichage classement

--- Championnat.py
+++ Championnat.py
@@ -1,5 +1,6 @@
 from match import Match
 from equipe import Equipe
+import operator
 
 class Championnat:
 
@@ -36,11 +37,27 @@ class Championnat:
         print("Date de début / fin : ", self.date_debut, " / ", self.date_fin, "\n")
     
     def afficher_classement(self):
+        tableauEquipe = []
+        
+        for equipe in self.equipes:
+            tableauEquipe.append([equipe, self.calculer_point(equipe)])
+        
+        sorted(tableauEquipe, key=operator.itemgetter(1), reverse=True)
+        
         print("Classement")
         print("\t\t Pts \t Joués \t Gagné \t Nul \t Perdu")
+        for equipe in tableauEquipe:
+            self.afficherEquipePoint(equipe[0], tableauEquipe.index(equipe) + 1)
+    
+    def afficherEquipePoint(self, equipe, classement):
+        print(str(classement), equipe.nom, self.calculer_point(equipe), equipe.mJoue, equipe.mGagne, equipe.mNul, equipe.mPerdu)
         
-        
-    def calculer_point(self):
+    def calculer_point(self, equipe):
+        point = 0
+        point += equipe.mGagne * self.point_gagne
+        point += equipe.mNul * self.point_nul
+        point += equipe.mPerdu * self.point_perdu
+        return point
         
         
     def ajouterEquipe(self, equipe):
