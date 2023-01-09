6dc98b1c1fbeb7e8ca445fe355fdf7342ee2c50b
CodingStrqf <l.ruy42@orange.fr>
2023-01-09 12:45:09 +0100

    Equipe

---
+++ equipe.py
@@ -0,0 +1,16 @@
+
+class Equipe:
+    def __init__(self, id, nom, date_creation, stade, entraineur, president):
+        self.id = id
+        self.nom = nom
+        self.date_creation = date_creation
+        self.stade = stade
+        self.entraineur = entraineur
+        self.president = president
+    
+    def afficher(self):
+        print("Equipe: ", self.nom)
+        print("Date de création: ", self.date_creation)
+        print("Stade: ", self.stade)
+        print("Entraineur: ", self.entraineur)
+        print("Président: ", self.president, "\n")
