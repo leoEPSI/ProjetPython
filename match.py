30455e5707b0a4b64cb9b40680b4e65683f5241c
CodingStrqf <l.ruy42@orange.fr>
2023-01-09 15:21:23 +0100

    correction

--- match.py
+++ match.py
@@ -2,8 +2,10 @@ from equipe import Equipe
 
 class Match:
     def __init__(self, score_equipe1, score_equipe2, numero_journee, equipe1, equipe2):
-        score_equipe1 = int(score_equipe1)
-        score_equipe2 = int(score_equipe2)
-        numero_journee = int(numero_journee)
-        equipe1 = str(equipe1)
-        equipe2 = str(equipe2)
+        self.score_equipe1 = int(score_equipe1)
+        self.score_equipe2 = int(score_equipe2)
+        self.numero_journee = int(numero_journee)
+        self.equipe1 = str(equipe1)
+        self.equipe2 = str(equipe2)
+        
+        
