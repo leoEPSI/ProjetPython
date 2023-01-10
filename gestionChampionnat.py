aa6bdfd6c6395994e0c45eb53969578ca190c646
CodingStrqf <l.ruy42@orange.fr>
2023-01-10 20:05:30 +0100

    test des entre

--- gestionChampionnat.py
+++ gestionChampionnat.py
@@ -1,6 +1,7 @@
 from equipe import Equipe
 from Championnat import Championnat
 from match import Match
+import os
 
 class GestionChampionnat:
     idEquipe = 0
@@ -11,6 +12,7 @@ class GestionChampionnat:
         self.championnats = []
         
     def menu(self):
+        cls()
         print("Que voulez-vous faire ?")
         print("1. Afficher les championnats")
         print("2. Ajouter un championnat")
@@ -20,7 +22,21 @@ class GestionChampionnat:
     
     # CHAMPIONNAT ###########################################
     def creerChampionnat(self):
-        championnat = Championnat(self.idChampionnat, str(input("Nom du championnat : ")), str(input("Date de début : ")), str(input("Date de fin : ")), int(input("Point gagné : ")), int(input("Point perdu : ")), int(input("Point nul : ")), int(input("Type de classement : ")))
+
+        try:
+            nom = str(inputTestStr("Nom du championnat : "))
+            dd = str(inputTestStr("Date de début : "))
+            df = str(inputTestStr("Date de fin : "))
+            pg = int(inputTestInt("Coefficient des point gagné : "))
+            pp = int(inputTestInt("Coefficient des point perdu : "))
+            pn = int(inputTestInt("Coefficient des point nul : "))
+            tc = int(inputTestInt("Type de classement : "))
+        except:
+            print("Erreur de saisie")
+            input("Appuyez sur une touche pour continuer")
+            return
+
+        championnat = Championnat(self.idChampionnat, nom, dd, df, pg, pp, pn, tc)
         self.idChampionnat += 1
         self.ajouterChampionnat(championnat)
         
@@ -30,32 +46,57 @@ class GestionChampionnat:
     
     # EQUIPE ###########################################
     def creerEquipe(self):
-        equipe = Equipe(self.idEquipe, str(input("Nom de l'équipe : ")), str(input("Date de création : ")), str(input("Stade : ")), str(input("Entraineur : ")), str(input("Président : ")))
-        self.idEquipe += 1
-        
-        championnat = str(input("Nom du championnat : "))
-        
-        championnatIndex = self.rechercheChampionnat(championnat) 
-        if championnatIndex != None:
-            self.championnats[championnatIndex].ajouterEquipe(equipe)
-        else:
-            print("Championnat inexistant")
+        self.afficherChampionnat()
 
+        nomChampionnat = str(inputTestStr("Nom du championnat : "))
+        championnatIndex = self.rechercheChampionnat(nomChampionnat)
+        if championnatIndex == None:
+            print("Championnat inexistant")
+            return
+
+        try:
+            ne = str(inputTestStr("Nom de l'équipe : "))
+            dc = str(inputTestStr("Date de création : "))
+            s = str(inputTestStr("Stade : "))
+            e = str(inputTestStr("Entraineur : "))
+            p = str(inputTestStr("Président : "))
+        except:
+            print("Erreur de saisie")
+            input("Appuyez sur une touche pour continuer")
+            return
+
+        equipe = Equipe(self.idEquipe, ne, dc, s, e, p)
+        self.idEquipe += 1
+
+        #le test est deja effectue au dessus
+        self.championnats[championnatIndex].ajouterEquipe(equipe)    
     ###########################################
     
     # MATCH ###########################################  
     def creerMatch(self):
-        match = Match(self.idMatch, int(input("Score de l'équipe 1 : ")), int(input("Score de l'équipe 2 : ")), str(input("Numéro de journée (1) : ")), str(input("Equipe 1 : ")), str(input("Equipe 2 : ")))
-        self.idMatch += 1
-        
-        championnat = str(input("Nom du championnat : "))
-        
-        championnatIndex = self.rechercheChampionnat(championnat) 
-        if championnatIndex != None:
-            self.championnats[championnatIndex].ajouterMatch(match)
-        else:
+        nomChampionnat = str(inputTestStr("Nom du championnat : "))
+        championnatIndex = self.rechercheChampionnat(nomChampionnat) 
+        if championnatIndex == None:
             print("Championnat inexistant")
-        
+            return
+        #affichage des equipes
+        self.afficherEquipes(nomChampionnat)
+
+        try:
+            equipeMatch1 = str(inputTestStr("Nom de l'équipe 1 : "))
+            equipeMatch2 = str(inputTestStr("Nom de l'équipe 2 : "))
+            scoreEquipe1 = int(inputTestInt("Score de l'équipe 1 : "))
+            scoreEquipe2 = int(inputTestInt("Score de l'équipe 2 : "))
+            nj = str(inputTestStr("Numéro de journée (1) : "))
+        except:
+            print("Erreur de saisie")
+            input("Appuyez sur une touche pour continuer")
+            return
+
+        match = Match(self.idMatch, scoreEquipe1, scoreEquipe2, nj, equipeMatch1, equipeMatch2)
+        self.idMatch += 1
+                
+        self.championnats[championnatIndex].ajouterMatch(match)
     ###########################################
     
     # retourne index du championnat et si pas trouvé retourne none
@@ -83,82 +124,103 @@ class GestionChampionnat:
         print("")    
 
     def afficherChampionnat(self):
+        cls()
         print("============================")
         print("Liste des championnats : ")
         print("============================")
         for championnat in self.championnats:
             if championnat.afficher() != None:
                 print(championnat.afficher())
+        input("Appuyez sur une touche pour continuer")
+
+    
+def inputTestStr(afficher):
+        valeur = input(afficher)
+        if valeur == None or valeur == "":
+            raise ValueError("Valeur incorrecte")
+
+        return valeur
+
+def inputTestInt(afficher):
+        valeur = input(afficher)
+        return valeur
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 #TESTS
 #CHAMPINNAT****************
-championnat = GestionChampionnat()
-championnat1 = Championnat(1, "Coupe du monde 2002", "13/04/2002", "13/05/2002", 3, 0, 1, 1)
-championnat2 = Championnat(2, "Coupe du monde 2006", "02/07/2006", "02/08/2006", 3, 0, 1, 2)
-championnat3 = Championnat(3, "Coupe d'Europe 2008", "25/10/2008", "25/11/2008", 3, 0, 1, 3)
+def test():
+    championnat = GestionChampionnat()
+    championnat1 = Championnat(1, "Coupe du monde 2002", "13/04/2002", "13/05/2002", 3, 0, 1, 1)
+    championnat2 = Championnat(2, "Coupe du monde 2006", "02/07/2006", "02/08/2006", 3, 0, 1, 2)
+    championnat3 = Championnat(3, "Coupe d'Europe 2008", "25/10/2008", "25/11/2008", 3, 0, 1, 3)
 
-championnat.ajouterChampionnat(championnat1)
-championnat.ajouterChampionnat(championnat2)
-championnat.ajouterChampionnat(championnat3)
+    championnat.ajouterChampionnat(championnat1)
+    championnat.ajouterChampionnat(championnat2)
+    championnat.ajouterChampionnat(championnat3)
 
-equipe1 = Equipe(1, "ST", "13/02/1987", "Stade 1", "Bruno", "Leo")
-equipe2 = Equipe(2, "OL", "05/09/1977", "Stade 2", "Theo", "Lucas")
-equipe3 = Equipe(3, "OM", "16/01/2002", "Stade 3", "Rayan", "Leon")
-equipe4 = Equipe(4, "PSG", "18/10/1995", "Stade 4", "Fred", "Marie Curie")
-equipe5 = Equipe(5, "OM2", "16/01/2002", "Stade 3", "Rayan2", "Leon2")
-equipe6 = Equipe(6, "OM3", "16/01/2002", "Stade 3", "Rayan3", "Leon3")
+    equipe1 = Equipe(1, "ST", "13/02/1987", "Stade 1", "Bruno", "Leo")
+    equipe2 = Equipe(2, "OL", "05/09/1977", "Stade 2", "Theo", "Lucas")
+    equipe3 = Equipe(3, "OM", "16/01/2002", "Stade 3", "Rayan", "Leon")
+    equipe4 = Equipe(4, "PSG", "18/10/1995", "Stade 4", "Fred", "Marie Curie")
+    equipe5 = Equipe(5, "OM2", "16/01/2002", "Stade 3", "Rayan2", "Leon2")
+    equipe6 = Equipe(6, "OM3", "16/01/2002", "Stade 3", "Rayan3", "Leon3")
 
-championnat1.ajouterEquipe(equipe1)
-championnat1.ajouterEquipe(equipe2)
-championnat1.ajouterEquipe(equipe5)
-championnat1.ajouterEquipe(equipe6)
-championnat2.ajouterEquipe(equipe3)
-championnat2.ajouterEquipe(equipe4)
-championnat3.ajouterEquipe(equipe3)
-championnat3.ajouterEquipe(equipe1)
+    championnat1.ajouterEquipe(equipe1)
+    championnat1.ajouterEquipe(equipe2)
+    championnat1.ajouterEquipe(equipe5)
+    championnat1.ajouterEquipe(equipe6)
+    championnat2.ajouterEquipe(equipe3)
+    championnat2.ajouterEquipe(equipe4)
+    championnat3.ajouterEquipe(equipe3)
+    championnat3.ajouterEquipe(equipe1)
 
-match1 = Match(1, 2, 1, equipe1, equipe2)
-match2 = Match(2, 2, 1, equipe2, equipe1)
-match3 = Match(3, 2, 1, equipe3, equipe4)
-match4 = Match(1, 2, 1, equipe4, equipe3)
-match5 = Match(1, 1, 1, equipe3, equipe1)
-match6 = Match(1, 3, 1, equipe1, equipe3)
-match7 = Match(1, 3, 1, equipe5, equipe6)
-match8 = Match(2, 3, 1, equipe2, equipe5)
+    match1 = Match(1, 2, 1, equipe1, equipe2)
+    match2 = Match(2, 2, 1, equipe2, equipe1)
+    match3 = Match(3, 2, 1, equipe3, equipe4)
+    match4 = Match(1, 2, 1, equipe4, equipe3)
+    match5 = Match(1, 1, 1, equipe3, equipe1)
+    match6 = Match(1, 3, 1, equipe1, equipe3)
+    match7 = Match(1, 3, 1, equipe5, equipe6)
+    match8 = Match(2, 3, 1, equipe2, equipe5)
+
+    championnat1.ajouterMatch(match1)
+    championnat1.ajouterMatch(match2)
+    championnat1.ajouterMatch(match7)
+    championnat1.ajouterMatch(match8)
+    championnat2.ajouterMatch(match3)
+    championnat2.ajouterMatch(match4)
+    championnat3.ajouterMatch(match5)
+    championnat3.ajouterMatch(match6)
+
+    championnat.afficherChampionnat()
+
+    championnat.afficherClassement("Coupe du monde 2002")
+    championnat.afficherClassement("Coupe du monde 2006")
+    championnat.afficherClassement("Coupe d'Europe 2008")
+
+    championnat.afficherEquipes("Coupe du monde 2002")
+    championnat.afficherEquipes("Coupe du monde 2006")
+    championnat.afficherEquipes("Coupe d'Europe 2008")
 
 
-championnat1.ajouterMatch(match1)
-championnat1.ajouterMatch(match2)
-championnat1.ajouterMatch(match7)
-championnat1.ajouterMatch(match8)
-championnat2.ajouterMatch(match3)
-championnat2.ajouterMatch(match4)
-championnat3.ajouterMatch(match5)
-championnat3.ajouterMatch(match6)
 
-championnat.afficherChampionnat()
+def main():
+    gestion = GestionChampionnat()
+    choix = 0
+    while choix != 5:
+        gestion.menu()
+        choix = int(input("Choix : "))
 
-championnat.afficherClassement("Coupe du monde 2002")
-championnat.afficherClassement("Coupe du monde 2006")
-championnat.afficherClassement("Coupe d'Europe 2008")
+        if choix == 1:
+            gestion.afficherChampionnat()
+        elif choix == 2:
+            gestion.creerChampionnat()
+        elif choix == 3:
+            gestion.creerEquipe()
+        elif choix == 4:
+            gestion.creerMatch()
 
-championnat.afficherEquipes("Coupe du monde 2002")
-championnat.afficherEquipes("Coupe du monde 2006")
-championnat.afficherEquipes("Coupe d'Europe 2008")
-
-# championnat.ajouterEquipe()
-# gestion = GestionChampionnat()
-# choix = 0
-
-# while choix != 5:
-#     gestion.menu()
-#     choix = int(input("Choix : "))
-
-#     if choix == 1:
-#         gestion.afficherChampionnat()
-#     elif choix == 2:
-#         gestion.ajouterChampionnat()
-#     elif choix == 3:
-#         gestion.ajouterEquipe()
-#     elif choix == 4:
-#         gestion.ajouterMatch()
+main()
+#test()
